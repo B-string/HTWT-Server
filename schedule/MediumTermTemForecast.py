fd34eba5037a66df989d6220abfcbcee33ad5d90
B-string <brightstring134@gmail.com>
2023-04-28 22:09:52 +0900

    2023/04/28 server commit

---
+++ schedule/MediumTermTemForecast.py
@@ -0,0 +1,4 @@
+
+
+class MediumTermTemForecastItem:
+    forecast: dict
--- schedule/test.py
+++ schedule/test.py
@@ -17,8 +17,7 @@ class ForecastService:
         page_no = 1
         num_of_rows = 1000
         data_type = "JSON"
-        today = dt.datetime.now()
-        (base_date, base_time) = ForecastService.forecast_time(today)
+        (base_date, base_time) = self.short_term_forecast_time()
         print(base_date, base_time)
 
         # 현재 기준 : 양재동
@@ -56,24 +55,59 @@ class ForecastService:
 
         self.manager.database_closing()
 
-    def forecast_time(today: dt) -> tuple:
+    def medium_term_tem_forecast(self):
+        url = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa"
+        page_no = 1
+        num_of_rows = 1000
+        data_type = "JSON"
+        tm_fc = self.medium_term_forecast_time()
+
+        # 현재 기준: 서울
+        reg_id = "11B10101"
+
+        params = {
+            "ServiceKey": Constant.api_key,
+            "pageNo": page_no,
+            "numOfRows": num_of_rows,
+            "dataType": data_type,
+            "regId": reg_id,
+            "tmFc": tm_fc
+        }
+
+        res = requests.get(url=url, params=params)
+        datas = res.json()
+        print(datas)
+
+    def short_term_forecast_time(self) -> tuple:
         today = dt.datetime.now()
         today_date = today.strftime("%Y%m%d")
         hour = int(today.strftime("%H"))
-        today_hour: str
+        today_time: str
         if hour >= 14 and hour <= 17:
-            today_hour = "1400"
+            today_time = "1400"
         elif hour >= 5 and hour <= 8:
-            today_hour = "0500"
+            today_time = "0500"
         else:
-            today_hour = "0500"
-        return (today_date, today_hour)
+            today_time = "0500"
+        return (today_date, today_time)
+
+    def medium_term_forecast_time(self) -> str:
+        today = dt.datetime.now()
+        today_date = today.strftime("%Y%m%d")
+        hour = int(today.strftime("%H"))
+        today_time: str
+        if hour >= 6 and hour < 18:
+            today_time = "0600"
+        else:
+            today_time = "1800"
+        return f"{today_date}{today_time}"
 
 
 def main():
     print("Hello World")
     test = ForecastService()
-    test.short_term_forecast()
+    # test.short_term_forecast()
+    test.medium_term_tem_forecast()
 
 
 if __name__ == "__main__":
